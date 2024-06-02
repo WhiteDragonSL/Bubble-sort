1ff25c75112b1c579dae5a8fb69efcecb198f5ff
WhiteDragonSL <imdpramuditha@gmail.com>
2024-06-02 07:25:26 +0530

    Update ModifiedBubbleSort.java

--- ModifiedBubbleSort.java
+++ ModifiedBubbleSort.java
@@ -9,7 +9,6 @@ public class ModifiedBubbleSort {
             swapped = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j + 1]
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
